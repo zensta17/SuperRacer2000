f61c86def2955202ff71e87bdaa0b2d2879275de
zensta17 <67415746+zensta17@users.noreply.github.com>
2020-11-26 14:50:48 +0100

    movement

---
+++ SuperRacer 2000/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 100f;
+    public float sidewaysForce = 900f;
+    public float jumpForce = 1000f;
+    public bool left;
+    public bool right;
+    public bool up;
+
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
