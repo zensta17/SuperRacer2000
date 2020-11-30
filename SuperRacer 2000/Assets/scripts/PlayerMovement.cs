bd7b7e6d986f939f7bb3de881aab499324d5266f
taumaa17 <72205559+taumaa17@users.noreply.github.com>
2020-11-30 18:51:04 +0100

    Erste richtige Demo
    Neue Sachen für Weltgenerierung eingebaut (Prefabs)
    Erstes spielbares Level
    Score System (Score.cs)

--- SuperRacer 2000/Assets/scripts/PlayerMovement.cs
+++ SuperRacer 2000/Assets/scripts/PlayerMovement.cs
@@ -1,31 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float forwardForce = 100f;
-    public float sidewaysForce = 900f;
+    public float sidewaysForce = 500f;
     public float jumpForce = 1000f;
     public bool left;
     public bool right;
     public bool up;
 
-
+ 
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
+        if(Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce, ForceMode.Acceleration);
+        }
+        Debug.Log(rb.velocity);
     }
 }
---
+++ SuperRacer 2000/Assets/scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public float playerStart;
+    public float currenPosition;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStart = player.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currenPosition = player.position.z;
+        scoreText.text = Math.Floor(Math.Abs(playerStart - currenPosition)).ToString();
+    }
+}
