48de397278519caff85dc7510dc146f7d3c59827
zensta17 <67415746+zensta17@users.noreply.github.com>
2021-03-18 08:27:57 +0100

    small shop update color ist veränderbar + bezahlung und speichern der auswahl
    this update is the best update

--- SuperRacer 2000/Assets/Scripts/Shop.cs
+++ SuperRacer 2000/Assets/Scripts/Shop.cs
@@ -5,38 +5,79 @@ using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
+    [SerializeField] public Material Material;
+
+    public bool redColorBought = false;
+    public bool blueColorBought = false;
+    public bool magentaColorBought = false;
+    public bool cyanColorBought = false;
+
+    public int kosten = 5;
+
+
+
     public static double coins;
     public Text CoinsAnzahl;
+
     void Start()
     {
-        PlayerData data = SaveSystem.LoadPlayer();
-        coins = data.coins;
-        CoinsAnzahl.text = Math.Truncate(data.coins).ToString();
+        Debug.Log(coins);
+        CoinsAnzahl.text = Math.Truncate(coins).ToString();
     }
     public void Quit()
     {
         Debug.Log("moin aus quit");
-        SceneManager.LoadScene("MenuScene");
+        SceneManager.LoadScene("MenuScene"); 
     }
 
     public void ButtonRed()
     {
         Debug.Log("Auswahl: Rot");
+
+        if ((coins >= kosten) && (redColorBought == false))
+        {
+            Material.color = Color.red;
+            coins = coins - kosten;
+            CoinsAnzahl.text = Math.Truncate(coins).ToString();
+            redColorBought = true;
+
+        }
     }
 
     public void ButtonBlue()
     {
         Debug.Log("Auswahl: Blau");
+
+        if ((coins >= kosten) && (blueColorBought == false))
+        {
+            Material.color = Color.blue;
+            coins = coins - kosten;
+            CoinsAnzahl.text = Math.Truncate(coins).ToString();
+            blueColorBought = true;
+        }
     }
 
-    public void ButtonPink()
+    public void ButtonMagenta()
     {
-        Debug.Log("Auswahl: Pink");
+        Debug.Log("Auswahl: Magenta");
+        if ((coins >= kosten) && (magentaColorBought == false))
+        {
+            Material.color = Color.magenta;
+            coins = coins - kosten;
+            CoinsAnzahl.text = Math.Truncate(coins).ToString();
+            magentaColorBought = true;
+        }
     }
-
     public void ButtonCyan()
     {
         Debug.Log("Auswahl: Cyan");
+        if ((coins >= kosten) && (cyanColorBought == false))
+        {
+            Material.color = Color.cyan;
+            coins = coins - kosten;
+            CoinsAnzahl.text = Math.Truncate(coins).ToString();
+            cyanColorBought = true;
+        }
     }
 }
 
